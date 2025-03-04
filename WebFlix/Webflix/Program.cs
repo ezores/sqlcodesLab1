05b2e12a60ba40b9530932d7b4a571dbb8139f1c
camylle.lupien.1@ens.etsmtl.ca <camylle.lupien.1@ens.etsmtl.com>
2025-03-03 23:47:15 -0500

    Add Repository Pattern for Film

--- WebFlix/Webflix/Program.cs
+++ WebFlix/Webflix/Program.cs
@@ -1,56 +1,23 @@
-﻿// using Avalonia;
-// using Avalonia.ReactiveUI;
-// using System;
-//
-// namespace Webflix;
-//
-// sealed class Program
-// {
-//     // Initialization code. Don't use any Avalonia, third-party APIs or any
-//     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-//     // yet and stuff might break.
-//     [STAThread]
-//     public static void Main(string[] args) => BuildAvaloniaApp()
-//         .StartWithClassicDesktopLifetime(args);
-//
-//     // Avalonia configuration, don't remove; also used by visual designer.
-//     public static AppBuilder BuildAvaloniaApp()
-//         => AppBuilder.Configure<App>()
-//             .UsePlatformDetect()
-//             .WithInterFont()
-//             .LogToTrace()
-//             .UseReactiveUI();
-// }
-
+﻿using Avalonia;
+using Avalonia.ReactiveUI;
 using System;
-using System.Linq;
 
-class Program
+namespace Webflix;
+
+sealed class Program
 {
-    static void Main()
-    {
-        using (var context = new MyDbContext())
-        {
-            try
-            {
-                var cartes = context.CartesCredit.ToList();
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
 
-                if (!cartes.Any())
-                {
-                    Console.WriteLine("No records found in CARTECREDIT.");
-                }
-                else
-                {
-                    foreach (var carte in cartes)
-                    {
-                        Console.WriteLine($"ID: {carte.CarteCreditId}, Numéro: {carte.Numero}, Expiration: {carte.DateExpiration.ToShortDateString()}, CVV: {carte.CVV}, Type: {carte.TypeCarte}");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error connecting to Oracle: " + ex.Message);
-            }
-        }
-    }
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace()
+            .UseReactiveUI();
 }
---
+++ WebFlix/Webflix/Services/FilmService.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Webflix.Models;
+using Webflix.Repositories.Interfaces;
+
+public class FilmService
+{
+    private readonly IFilmRepository _filmRepository;
+    
+    public FilmService(IFilmRepository filmRepository)
+    {
+        _filmRepository = filmRepository;
+    }
+    
+    public async Task<Film> GetFilmDetailsAsync(int id)
+    {
+        return await _filmRepository.GetByIdAsync(id);
+    }
+    
+    public async Task<IEnumerable<Film>> SearchFilmsAsync(string searchTerm)
+    {
+        return await _filmRepository.SearchByTitleAsync(searchTerm);
+    }
+    
+    public async Task<IEnumerable<Film>> AdvancedSearchAsync(
+        string title, int? year, string genre, 
+        int? actorId, int? directorId, 
+        string language, int? countryId)
+    {
+        return await _filmRepository.SearchAdvancedAsync(
+            title, year, genre, actorId, directorId, language, countryId);
+    }
+}
