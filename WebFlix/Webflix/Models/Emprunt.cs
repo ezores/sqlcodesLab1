7ec0a69a8c74676b129a3d254bc67f6da86eca78
camylle.lupien.1@ens.etsmtl.ca <camylle.lupien.1@ens.etsmtl.com>
2025-03-03 23:11:33 -0500

    Add missing models classes pt. 2

---
+++ WebFlix/Webflix/Models/Emprunt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Webflix.Models
+{
+    [Table("Emprunt")]
+    public class Emprunt
+    {
+        [Key]
+        [Column("copieId")]
+        public int CopieId { get; set; }
+        
+        [Key]
+        [Column("nomUsager")]
+        [StringLength(50)]
+        public string NomUsager { get; set; }
+        
+        [Required]
+        [Column("dateDebutEmprunt")]
+        public DateTime DateDebutEmprunt { get; set; }
+        
+        // Navigation property
+        [ForeignKey("CopieId")]
+        public virtual CopieFilm Copie { get; set; }
+    }
+}
