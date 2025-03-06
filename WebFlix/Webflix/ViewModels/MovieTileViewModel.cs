739f5e049ef593cdb704e8ada116e30d326a8325
mormax23 <maxime-lamontage.morrissette@humanware.com>
2025-03-05 22:56:11 -0500

    wip

--- WebFlix/Webflix/ViewModels/MovieTileViewModel.cs
+++ WebFlix/Webflix/ViewModels/MovieTileViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace Webflix.ViewModels;
+﻿using ReactiveUI;
+
+namespace Webflix.ViewModels;
 
 public class MovieTileViewModel : ViewModelBase
 {
-    
+    private string _movieTitle = string.Empty;
+
+    public string MovieTitle
+    {
+        get => _movieTitle;
+        set => this.RaiseAndSetIfChanged(ref _movieTitle, value);
+    }
+
+    //TODO: handle images (see doc)
 }
